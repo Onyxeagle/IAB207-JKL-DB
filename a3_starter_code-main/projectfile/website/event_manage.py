242ee1400b8d71738abaeaaa6f29226c69805ec5
Daiquerry <110011064+Daiquerry@users.noreply.github.com>
2023-10-11 11:57:38 +1000

    Added file pathing function
    The function check_upload _file() was added. This is used to manage the delivery of files users upload in the /static/img. This function will be used for the create and edit event pages.

--- a3_starter_code-main/projectfile/website/event_manage.py
+++ a3_starter_code-main/projectfile/website/event_manage.py
@@ -15,14 +15,22 @@ def event_create():
     createForm = CreateEditForm()
     return render_template('event_manage/create_event.html', form=createForm)
 
-
-
 # creates the route for the event edit page
 @eventbp.route('/edit_event', methods=['GET','POST'])
 def event_edit():
     editForm = CreateEditForm()
     deleteForm = DeleteForm()
     return render_template('event_manage/edit_event.html', form=editForm, form2=deleteForm)
+
+# a function that is used to direct the directory pathway for the image the user uploads
+def check_upload_file(form):
+    fp = form.eventImage.data
+    filename = fp.filename
+    BASE_PATH = os.path.dirname(__file__)
+    upload_path = os.path.join(BASE_PATH, 'static/img', secure_filename(filename))
+    db_upload_path = '/static/img/' + secure_filename(filename)
+    fp.save(upload_path)
+    return db_upload_path
 
 # creates a route that will display the user's events that they have posted and are editable 
 @eventbp.route('/editable_events', methods=['GET','POST'])
