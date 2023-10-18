641452abc1c3d4871e89f837ab938ee47294a135
Daiquerry <110011064+Daiquerry@users.noreply.github.com>
2023-10-18 13:09:31 +1000

    Added search function
    Search function has been added to views.py.
    Has yet to be tested with search function.

--- a3_starter_code-main/projectfile/website/views.py
+++ a3_starter_code-main/projectfile/website/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Events
+from . import db
 
 mainbp = Blueprint('main', __name__)
 
@@ -6,3 +8,17 @@ mainbp = Blueprint('main', __name__)
 @mainbp.route('/')
 def index():
     return render_template('index.html')
+
+# identifies the search route and the forms to be displayed
+# has an if statement that will also account for user search inputs that will return events that contain what they enter into the text field
+# if nothing has been found it returns to index making it look like nothing has happened 
+@mainbp.route('/search')
+def search():
+    if (request.args['search'] and request.args['search'] != ""):
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        events = db.session.scalars(db.select(Events).where(Events.name.like(query) or db.select(Events).where(Events.description).like(query) or db.select(Events).where(Events.genre).like(query)))
+        print(events)
+        return render_template('index.html', events=events)
+    else:
+        return redirect(url_for('main.index'))
