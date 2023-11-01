7158f3320cefc1eae1e27e63aa9cf1e4a5936c05
Joshua Kyle-Little <joshkylelittle@gmail.com>
2023-11-01 17:01:59 +1000

    dynamic event display added, search bug fixed

--- a3_starter_code-main/projectfile/website/views.py
+++ a3_starter_code-main/projectfile/website/views.py
@@ -8,7 +8,8 @@ mainbp = Blueprint('main', __name__)
 # the default route landing page that will display index.html
 @mainbp.route('/')
 def index():
-    return render_template('index.html')
+    event_list = Events.query.all()
+    return render_template('index.html', event_list = event_list)
 
 # identifies the search route and the forms to be displayed
 # has an if statement that will also account for user search inputs that will return events that contain what they enter into the text field
@@ -21,9 +22,9 @@ def search():
     if (request.args['search'] and request.args['search'] != ""):
         print(request.args['search'])
         query = "%" + request.args['search'] + "%"
-        events = db.session.scalars(db.select(Events).filter(
+        event_list = db.session.scalars(db.select(Events).filter(
             or_(Events.name.like(query), Events.description.like(query), Events.genre.like(query))))
-        print(events)
-        return render_template('index.html', events=events)
+        print(event_list)
+        return render_template('index.html', event_list = event_list)
     else:
         return redirect(url_for('main.index'))
